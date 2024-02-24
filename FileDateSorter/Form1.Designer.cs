0854b6fa81d69032c54d8acd97167fe40cf276f3
torfel2811 <torsten.feller@web.de>
2024-02-24 22:03:41 +0100

    First Commit

---
+++ FileDateSorter/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FileDateSorter
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Erforderliche Designervariable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Verwendete Ressourcen bereinigen.
+    /// </summary>
+    /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Vom Windows Form-Designer generierter Code
+
+    /// <summary>
+    /// Erforderliche Methode für die Designerunterstützung.
+    /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.OFD1 = new System.Windows.Forms.OpenFileDialog();
+      this.FBD1 = new System.Windows.Forms.FolderBrowserDialog();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.StatusBar = new System.Windows.Forms.StatusStrip();
+      this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+      this.pbImage = new System.Windows.Forms.PictureBox();
+      this.menuStrip1.SuspendLayout();
+      this.StatusBar.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+      this.menuStrip1.Size = new System.Drawing.Size(637, 24);
+      this.menuStrip1.TabIndex = 0;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+      this.fileToolStripMenuItem.Text = "File";
+      // 
+      // openFolderToolStripMenuItem
+      // 
+      this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+      this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+      this.openFolderToolStripMenuItem.Text = "Open Files";
+      this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolderToolStripMenuItem_Click);
+      // 
+      // StatusBar
+      // 
+      this.StatusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+      this.StatusBar.Location = new System.Drawing.Point(0, 549);
+      this.StatusBar.Name = "StatusBar";
+      this.StatusBar.Size = new System.Drawing.Size(637, 22);
+      this.StatusBar.TabIndex = 1;
+      // 
+      // toolStripStatusLabel1
+      // 
+      this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+      this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+      // 
+      // pbImage
+      // 
+      this.pbImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.pbImage.Location = new System.Drawing.Point(0, 27);
+      this.pbImage.Name = "pbImage";
+      this.pbImage.Size = new System.Drawing.Size(637, 519);
+      this.pbImage.TabIndex = 2;
+      this.pbImage.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(637, 571);
+      this.Controls.Add(this.pbImage);
+      this.Controls.Add(this.StatusBar);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Margin = new System.Windows.Forms.Padding(2);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.StatusBar.ResumeLayout(false);
+      this.StatusBar.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.OpenFileDialog OFD1;
+    private System.Windows.Forms.FolderBrowserDialog FBD1;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+    private System.Windows.Forms.StatusStrip StatusBar;
+    private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    private System.Windows.Forms.PictureBox pbImage;
+  }
+}
+
---
+++ FileDateSorter/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace FileDateSorter
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    string m_Statustext;
+
+    public string Statustext
+    {
+      get { return m_Statustext; }
+      set { m_Statustext = value; }
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+    }
+
+    private void openFolderToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        OFD1.Filter = "Image(*.jpg)|*.jpg|Image(*.jpeg)|*.jpeg|Movie(*.mp4)|*.mp4|Movie(*.3gp)|*.3gp|AVI(*.avi)|*.avi|MOV(*.mov)|*.mov|Nikon RAW(*.nef)|*.nef|HiEff(*.heic)|*.heic";
+        OFD1.Multiselect = true;
+      if (OFD1.ShowDialog() == DialogResult.OK)
+      {
+          Thread CopyThread = new Thread(CopyFiles);
+          CopyThread.Start();
+      }
+    }
+
+    private void CopyFiles()
+    {
+      String[] strFilnames;
+      DateTime dtDateTime;
+      strFilnames = new string[OFD1.SafeFileNames.Length];
+      strFilnames = OFD1.FileNames;
+      DirectoryInfo DirInfo = new DirectoryInfo(OFD1.FileName);
+      string sText;
+      int FileIndex = 1;
+     //menuStrip1.Enabled = false;
+      foreach (string index in strFilnames)
+      {
+        
+        dtDateTime = File.GetLastWriteTime(index);
+        FileInfo FI = new FileInfo(index);
+        sText = dtDateTime.Year + ".";
+        if (dtDateTime.Month < 10)
+          sText = sText + "0" + dtDateTime.Month + ".";
+        else
+          sText = sText + dtDateTime.Month + ".";
+        if (dtDateTime.Day < 10)
+            sText = sText + "0" + dtDateTime.Day + "\\Camera\\";
+        else
+          sText = sText + dtDateTime.Day + "\\Camera\\";
+        //Bitmap bmp1 = (Bitmap)Bitmap.FromFile(index);
+        //Bitmap bmp2 = new Bitmap(bmp1, (int)(bmp1.Width * 0.2), (int)(bmp1.Height * 0.2));
+        //pbImage.Image = bmp2;
+        
+        try
+        {
+          if (!Directory.Exists(FI.DirectoryName + "\\" + sText))
+            Directory.CreateDirectory(FI.DirectoryName + "\\" + sText);
+        }
+        catch (IOException ex)
+        {
+          MessageBox.Show("Ordner konnte nicht erstellt werden!\n"+ex.ToString());
+        }
+        try
+        {
+          pbImage.Image = null;
+          File.Move(index, FI.DirectoryName + "\\" + sText + "\\" + FI.Name);
+        }
+        catch (IOException ex)
+        {
+          MessageBox.Show("Datei " + index + " konte nicht kopiert werden!\n" + ex.ToString());
+        }
+        Statustext = Convert.ToString((FileIndex * 100) / OFD1.FileNames.Length ) + " % fertig!";
+        StatusBar.Invoke(new MethodInvoker (SetStatus));
+        FileIndex++;
+      }
+      
+      
+      MessageBox.Show("Fertig!");
+
+    }
+    private void SetStatus()
+    {
+      toolStripStatusLabel1.Text = Statustext;
+    }
+  }
+}
